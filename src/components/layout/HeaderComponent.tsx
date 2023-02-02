9ea85f4e7b7a10ab77df7cedb7cb46c151fed3fe
jeffrey125 <jeffreyasilo8@gmail.com>
2023-02-03 00:56:43 +0800

    feat: added Login button and simple logic

--- src/components/layout/HeaderComponent.tsx
+++ src/components/layout/HeaderComponent.tsx
@@ -1,4 +1,4 @@
-import { createStyles, Header, Container } from '@mantine/core';
+import { createStyles, Header, Container, Button } from '@mantine/core';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 
@@ -10,6 +10,7 @@ const useStyles = createStyles((theme) => ({
     height: '100%',
     maxWidth: '100%',
     backgroundColor: theme.colors.blue[5],
+    marginBottom: 0,
   },
 
   links: {
@@ -34,8 +35,20 @@ export const HeaderComponent = () => {
   const { classes } = useStyles();
   const router = useRouter();
 
+  const imageClickHandler = () => {
+    // TODO put the isAuthenticathed flag here to have a guard return
+
+    void router.push('/');
+  };
+
+  const loginLogoutButton = () => {
+    // TODO put the isAuthenticathed flag here to have a login logout logic
+
+    void router.push('/account/login');
+  };
+
   return (
-    <Header height={60} mb={120}>
+    <Header height={60}>
       <Container
         className={classes.header}
         sx={{ paddingLeft: 80, paddingRight: 80 }}>
@@ -46,14 +59,18 @@ export const HeaderComponent = () => {
           width={42}
           height={42}
           alt='My Logo'
-          onClick={() => {
-            // TODO put the isAuthenticathed flag here to have a guard return
-
-            void router.push('/');
-          }}
+          onClick={imageClickHandler}
         />
 
-        <button>Login</button>
+        <Button
+          size='md'
+          color='green'
+          role='button'
+          component='a'
+          onClick={loginLogoutButton}
+          sx={{ width: 160 }}>
+          Login
+        </Button>
       </Container>
     </Header>
   );
