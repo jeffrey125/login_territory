7ba007db1119c117f60ea65a5209b5772fe9590f
jeffrey125 <jeffreyasilo8@gmail.com>
2023-02-02 23:53:29 +0800

    chore: remove rtl-cache

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import { Global, MantineProvider } from '@mantine/core';
 import { NotificationsProvider } from '@mantine/notifications';
 import Head from 'next/head';
 
-import { rtlCache } from '@src/rtl-chache';
 import type { AppProps } from 'next/app';
 
 const App = ({ Component, pageProps }: AppProps) => {
@@ -14,66 +13,63 @@ const App = ({ Component, pageProps }: AppProps) => {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <div dir='rtl'>
-        <Global
-          styles={(theme) => ({
-            '*, *::before, *::after': {
-              boxSizing: 'border-box',
+
+      <Global
+        styles={(theme) => ({
+          '*, *::before, *::after': {
+            boxSizing: 'border-box',
+          },
+
+          html: {
+            height: '100%',
+            scrollBehavior: 'smooth',
+          },
+
+          '#__next': {
+            position: 'relative',
+            display: 'flex',
+            flexDirection: 'column',
+            minHeight: '100vh',
+            margin: 0,
+          },
+
+          section: {
+            maxWidth: 1536,
+            marginTop: 0,
+            marginBottom: 0,
+            marginLeft: 20,
+            marginRight: 20,
+
+            [theme.fn.largerThan('sm')]: {
+              marginLeft: 40,
+              marginRight: 40,
             },
 
-            html: {
-              height: '100%',
-              scrollBehavior: 'smooth',
+            [theme.fn.largerThan('md')]: {
+              marginLeft: 80,
+              marginRight: 80,
             },
 
-            '#__next': {
-              position: 'relative',
-              display: 'flex',
-              flexDirection: 'column',
-              minHeight: '100vh',
-              margin: 0,
+            [`@media (min-width: 1600px)`]: {
+              marginLeft: 'auto',
+              marginRight: 'auto',
             },
-
-            section: {
-              maxWidth: 1536,
-              marginTop: 0,
-              marginBottom: 0,
-              marginLeft: 20,
-              marginRight: 20,
-
-              [theme.fn.largerThan('sm')]: {
-                marginLeft: 40,
-                marginRight: 40,
-              },
-
-              [theme.fn.largerThan('md')]: {
-                marginLeft: 80,
-                marginRight: 80,
-              },
-
-              [`@media (min-width: 1600px)`]: {
-                marginLeft: 'auto',
-                marginRight: 'auto',
-              },
-            },
-          })}
-        />
-        <MantineProvider
-          withNormalizeCSS
-          theme={{
-            colorScheme: 'light',
-            focusRing: 'auto',
-            fontFamily: 'Roboto, sans-serif',
-            dir: 'rtl',
-          }}
-          emotionCache={rtlCache}>
-          <NotificationsProvider>
-            <main>
-              <Component {...pageProps} />
-            </main>
-          </NotificationsProvider>
-        </MantineProvider>
-      </div>
+          },
+        })}
+      />
+      <MantineProvider
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'light',
+          focusRing: 'auto',
+          fontFamily: 'Roboto, sans-serif',
+        }}>
+        <NotificationsProvider>
+          <main>
+            <Component {...pageProps} />
+          </main>
+        </NotificationsProvider>
+      </MantineProvider>
     </>
   );
 };
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,24 +1,15 @@
-import Document from 'next/document';
-import { ServerStyles, createStylesServer } from '@mantine/next';
-import { rtlCache } from '@src/rtl-chache';
-import type { DocumentContext } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document';
 
-const stylesServer = createStylesServer(rtlCache);
+const Document = () => {
+  return (
+    <Html lang='en'>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
 
-export default class _Document extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
-
-    return {
-      ...initialProps,
-      styles: [
-        initialProps.styles,
-        <ServerStyles
-          html={initialProps.html}
-          server={stylesServer}
-          key='styles'
-        />,
-      ],
-    };
-  }
-}
+export default Document;
