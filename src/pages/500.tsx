bc1c480e0f76f0d57bd05709fa91d96149402de9
jeffrey125 <jeffreyasilo8@gmail.com>
2023-02-02 23:37:55 +0800

    feat: added error 500 page

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,5 +1,79 @@
+import {
+  createStyles,
+  Title,
+  Text,
+  Button,
+  Container,
+  Group,
+} from '@mantine/core';
+import { useRouter } from 'next/router';
+
+const useStyles = createStyles((theme) => ({
+  root: {
+    height: '100vh',
+    display: 'flex',
+    alignItems: 'center',
+    backgroundColor: theme.fn.variant({
+      variant: 'filled',
+      color: theme.primaryColor,
+    }).background,
+  },
+
+  label: {
+    textAlign: 'center',
+    fontWeight: 900,
+    fontSize: 220,
+    lineHeight: 1,
+    marginBottom: theme.spacing.xl * 1.5,
+    color: theme.colors[theme.primaryColor][3],
+
+    [theme.fn.smallerThan('sm')]: {
+      fontSize: 120,
+    },
+  },
+
+  title: {
+    fontFamily: `${theme.fontFamily || 'sans-serif'}`,
+    textAlign: 'center',
+    fontWeight: 900,
+    fontSize: 38,
+    color: theme.white,
+
+    [theme.fn.smallerThan('sm')]: {
+      fontSize: 32,
+    },
+  },
+
+  description: {
+    maxWidth: 540,
+    margin: 'auto',
+    marginTop: theme.spacing.xl,
+    marginBottom: theme.spacing.xl * 1.5,
+    color: theme.colors[theme.primaryColor][1],
+  },
+}));
+
 const Custom500 = () => {
-  return <h1>Server Error!</h1>;
+  const { classes } = useStyles();
+  const router = useRouter();
+
+  return (
+    <div className={classes.root}>
+      <Container>
+        <div className={classes.label}>500</div>
+        <Title className={classes.title}>Something bad just happened...</Title>
+        <Text size='lg' align='center' className={classes.description}>
+          Our servers could not handle your request. Don&apos;t worry, our
+          development team was already notified. Try refreshing the page.
+        </Text>
+        <Group position='center'>
+          <Button variant='white' size='md' onClick={() => router.reload()}>
+            Refresh the page
+          </Button>
+        </Group>
+      </Container>
+    </div>
+  );
 };
 
 export default Custom500;
