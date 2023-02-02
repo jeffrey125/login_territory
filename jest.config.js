3b4ae234da3494dd00bc957528ff0bcec3fab762
jeffrey125 <jeffreyasilo8@gmail.com>
2023-02-02 21:53:12 +0800

    config: added territories in datasets
    - Configured also jest on testPathIgnorePatterns

--- jest.config.js
+++ jest.config.js
@@ -13,6 +13,10 @@ const customJestConfig = {
   },
   testEnvironment: 'jest-environment-jsdom',
   moduleDirectories: ['node_modules', 'src'],
+  testPathIgnorePatterns: [
+    '<rootDir>/src/__tests__/datasets',
+    '<rootDir>/node_modules',
+  ],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
