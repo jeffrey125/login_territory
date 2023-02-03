a14b662ce6d4e617b8e36cc0ef7a8094fe5bf46d
jeffrey125 <jeffreyasilo8@gmail.com>
2023-02-03 21:26:03 +0800

    feat: added home reroute

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,32 @@
+import { useEffect } from 'react';
+import { useIsLoggedIn } from '@src/hooks/useIsLoggedIn';
+import Router from 'next/router';
+import { Container } from '@mantine/core';
+
 const Home = () => {
-  return <h1>Territories Page</h1>;
+  const { isLoggedIn } = useIsLoggedIn();
+
+  useEffect(() => {
+    if (isLoggedIn) return;
+
+    void Router.push('/account/login');
+  }, [isLoggedIn]);
+
+  return (
+    <Container
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        height: '100vh',
+        width: '100%',
+      }}>
+      <h1>Territories Page</h1>
+    </Container>
+  );
 };
 
 export default Home;
+
+// TODO Try to implement this on SSR
